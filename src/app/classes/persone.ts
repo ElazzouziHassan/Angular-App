7275ed26703f7df03e6aa2362bd5d830d6392918
wizardy <wizardhassan99@gmail.com>
2023-12-18 21:35:46 +0100

    commit

--- src/app/classes/persone.ts
+++ src/app/classes/persone.ts
@@ -1,2 +1,25 @@
 export class Persone {
+  constructor(private _num?: number, private _nom?: string, private _prenom?: string
+    ){
+  
+    }
+  
+    get num() {
+      return this._num ?? 0 ;
+    }
+    set num(_num: number) {
+      this._num = _num;
+    }
+    get nom() {
+      return this._nom ?? "";
+    }
+    set nom(_nom: string) {
+      this._nom = this._nom;
+    }
+    get prenom() {
+      return this._prenom ?? "";
+    }
+    set prenom(_prenom: string) {
+      this._prenom = this._prenom;
+    }
 }
