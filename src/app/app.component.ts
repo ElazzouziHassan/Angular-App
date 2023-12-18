4b7db6cd338398bd691442cba9d841c60ce72ac9
wizardy <wizardhassan99@gmail.com>
2023-12-18 21:39:07 +0100

    commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { Personne } from './classes/personne';
 
 @Component({
   selector: 'app-root',
@@ -11,5 +12,5 @@ import { RouterOutlet } from '@angular/router';
 })
 export class AppComponent {
   title = 'Angular App | Tutorial';
-  personne: Personnne = new Personne(1, 'El azzouzi', 'Hassan');
+  personne: Personne = new Personne(1, 'El azzouzi', 'Hassan');
 }
