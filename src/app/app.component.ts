64436c3b01c88bd5aafcfe3881db5084218cd336
wizardy <wizardhassan99@gmail.com>
2023-12-25 10:29:09 +0100

    commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RouterOutlet } from '@angular/router';
+import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
 import { Personne } from './classes/personne';
 import { MenuComponent } from './component/menu/menu.component';
 import { AdresseComponent } from './component/adresse/adresse.component';
 import { StagiaireComponent } from './component/stagiaire/stagiaire.component';
+import { AboutComponent } from './component/about/about.component';
+import { ContactComponent } from './component/contact/contact.component';
+import { ServicesComponent } from './component/services/services.component';
+import { FooterComponent } from './component/footer/footer.component';
 
 @Component({
   selector: 'app-root',
@@ -14,7 +18,13 @@ import { StagiaireComponent } from './component/stagiaire/stagiaire.component';
     RouterOutlet,
     MenuComponent,
     AdresseComponent,
-    StagiaireComponent
+    StagiaireComponent,
+    AboutComponent,
+    ContactComponent,
+    ServicesComponent,
+    FooterComponent,
+    RouterLink,
+    RouterLinkActive
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
