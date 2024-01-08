1805d4b69fa5baac0d0028e24b8dddd077da7e42
wizardy <wizardhassan99@gmail.com>
2024-01-08 10:24:52 +0100

    commit

--- src/app/component/heroes-list/heroes-list.component.ts
+++ src/app/component/heroes-list/heroes-list.component.ts
@@ -1,9 +1,13 @@
 import { Component } from '@angular/core';
+import { RouterLink, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-heroes-list',
   standalone: true,
-  imports: [],
+  imports: [
+    RouterOutlet,
+  RouterLink
+  ],
   templateUrl: './heroes-list.component.html',
   styleUrl: './heroes-list.component.css'
 })
