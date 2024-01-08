9ab2467c7256a8d6cc3b2b9b3d54e601bd78e09f
wizardy <wizardhassan99@gmail.com>
2024-01-08 10:24:41 +0100

    commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -8,16 +8,31 @@ import { ContactComponent } from './component/contact/contact.component';
 import { HomeComponent } from './pages/home/home.component';
 import { CrisisListComponent } from './component/crisis-list/crisis-list.component';
 import { HeroesListComponent } from './component/heroes-list/heroes-list.component';
+import { PageNotFoundComponent } from './pages/page-not-found/page-not-found.component';
+import { ChildAComponent } from './component/heroseComponent/child-a/child-a.component';
+import { ChildBComponent } from './component/heroseComponent/child-b/child-b.component';
 
 export const routes: Routes = [
+  {path:'', redirectTo: 'heroes', pathMatch: 'full'},
   {path:'home', component: HomeComponent},
   {path:'crisis', component: CrisisListComponent},
-  {path:'heroes', component: HeroesListComponent},
+  {
+    path:'heroes', component: HeroesListComponent,
+    children: [
+      {
+        path: 'child-a', component: ChildAComponent
+      },
+      {
+        path: 'child-b', component: ChildBComponent
+      }
+    ]
+  },
   {path:'stagiaire', component: StagiaireComponent},
   {path:'adresse', component: AdresseComponent},
   {path:'services', component: ServicesComponent},
   {path:'about', component: AboutComponent},
   {path:'contact', component: ContactComponent},
+  {path:'**', component: PageNotFoundComponent},
 ];
 
 @NgModule({
