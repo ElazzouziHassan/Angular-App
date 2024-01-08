7db72b6bba0566394739f46af7a5210ac170689c
wizardy <wizardhassan99@gmail.com>
2024-01-08 09:22:53 +0100

    commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,8 +5,14 @@ import { NgModule } from '@angular/core';
 import { ServicesComponent } from './component/services/services.component';
 import { AboutComponent } from './component/about/about.component';
 import { ContactComponent } from './component/contact/contact.component';
+import { HomeComponent } from './pages/home/home.component';
+import { CrisisListComponent } from './component/crisis-list/crisis-list.component';
+import { HeroesListComponent } from './component/heroes-list/heroes-list.component';
 
 export const routes: Routes = [
+  {path:'home', component: HomeComponent},
+  {path:'crisis', component: CrisisListComponent},
+  {path:'heroes', component: HeroesListComponent},
   {path:'stagiaire', component: StagiaireComponent},
   {path:'adresse', component: AdresseComponent},
   {path:'services', component: ServicesComponent},
